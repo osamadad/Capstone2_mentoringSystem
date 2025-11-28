0d4cb75c2f1f109700bdfc6c4f93ce9645bf817d
osama dad <osamadad768@gmail.com>
2025-11-28 19:45:42 +0300

    feat: small change in admin repository

--- src/main/java/com/tuwaiq/capstone2_mentoringsystem/Repository/AdminRepository.java
+++ src/main/java/com/tuwaiq/capstone2_mentoringsystem/Repository/AdminRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AdminRepository extends JpaRepository<Admin,Integer> {
 
-    Admin findAdminsById(Integer id);
+    Admin findAdminById(Integer id);
 }
