cfdd643a932e87b18b9ee1118313d162c2caf57c
osama dad <osamadad768@gmail.com>
2025-11-28 09:32:12 +0300

    initial commit

---
+++ src/main/java/com/tuwaiq/capstone2_mentoringsystem/Capstone2MentoringSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tuwaiq.capstone2_mentoringsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capstone2MentoringSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Capstone2MentoringSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tuwaiq/capstone2_mentoringsystem/Capstone2MentoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuwaiq.capstone2_mentoringsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Capstone2MentoringSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
