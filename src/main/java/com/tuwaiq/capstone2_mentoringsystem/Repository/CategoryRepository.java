781f294e069dc3d3c13583d085f7dee5284456b0
osama dad <osamadad768@gmail.com>
2025-11-30 14:50:42 +0300

    feat: delete bugs in category

--- src/main/java/com/tuwaiq/capstone2_mentoringsystem/Repository/CategoryRepository.java
+++ src/main/java/com/tuwaiq/capstone2_mentoringsystem/Repository/CategoryRepository.java
@@ -2,7 +2,11 @@ package com.tuwaiq.capstone2_mentoringsystem.Repository;
 
 import com.tuwaiq.capstone2_mentoringsystem.Models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Integer> {
